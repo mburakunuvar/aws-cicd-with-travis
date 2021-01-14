9c2a051bf64eaba20559f526a73e4c14d4f2defc
Burak Unuvar <buraku@amazon.com>
2021-01-14 10:03:56 +0100

    updated images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import diagram from "./aws-ci-cd-with-travis.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={aws-ci-cd-with-travis} className="App-logo" alt="diagram" />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.travis-ci.com/user/deployment/elasticbeanstalk/"
           target="_blank"
           rel="noopener noreferrer"
         >
