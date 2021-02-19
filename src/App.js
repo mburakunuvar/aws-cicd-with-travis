6c5b4ec5d4be6f25c6191103793b5a6a79bd4a95
Burak Unuvar <buraku@amazon.com>
2021-02-19 12:49:50 +0100

    initializa demo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          AWS Elastic Beanstalk CI/CD PIPELINE WITH TRAVIS
+          Demo:AWS Elastic Beanstalk CI/CD PIPELINE WITH TRAVIS
         </a>
       </header>
     </div>
