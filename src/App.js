4fb84a070c9652917839c552b6de9fc7d50f81dd
Burak <mburakunuvar@gmail.com>
2021-09-16 22:48:12 +0200

    init demo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CAPGEMINI - Demo:AWS Elastic Beanstalk CI/CD PIPELINE WITH TRAVIS
+          {/* CAPGEMINI - Demo:AWS Elastic Beanstalk CI/CD PIPELINE WITH TRAVIS */}
         </a>
       </header>
     </div>
